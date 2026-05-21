401d3cfdfc64de4b31d4ba320bd67979216f8ae3
lakikishorsubba <lakikishor7@gmail.com>
2026-05-21 10:17:20 +0600

    fix earch rotate

--- src/pages/home/GlowBackground.js
+++ src/pages/home/GlowBackground.js
@@ -5,6 +5,22 @@ import "./style.css";
 const GlowBackground = () => {
   return (
     <div className="glow-background">
+      {/* Stars Background */}
+      <div className="stars-container">
+        {[...Array(50)].map((_, i) => (
+          <div 
+            key={i} 
+            className="star" 
+            style={{
+              top: `${Math.random() * 100}%`,
+              left: `${Math.random() * 100}%`,
+              animationDelay: `${Math.random() * 5}s`,
+              opacity: Math.random()
+            }}
+          />
+        ))}
+      </div>
+
       <motion.div
         className="blob blob-1"
         animate={{
@@ -31,19 +47,25 @@ const GlowBackground = () => {
           ease: "linear",
         }}
       />
-      <motion.div
-        className="blob blob-3"
-        animate={{
-          scale: [1, 1.1, 1],
-          x: [0, 80, 0],
-          y: [0, -100, 0],
-        }}
-        transition={{
-          duration: 18,
-          repeat: Infinity,
-          ease: "linear",
-        }}
-      />
+      
+      {/* Earth Portal Background */}
+      <div className="earth-portal-wrapper">
+        <motion.div 
+          className="earth-container background-earth"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <div className="earth">
+            <div className="earth-texture"></div>
+          </div>
+          <div className="earth-overlay"></div>
+          <div className="portal-ring ring-1"></div>
+          <div className="portal-ring ring-2"></div>
+          <div className="portal-ring ring-3"></div>
+        </motion.div>
+      </div>
+
       <div className="glass-overlay" />
     </div>
   );
